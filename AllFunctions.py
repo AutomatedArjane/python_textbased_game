40e50bcc45366fd1bd6730a8bbf67c8a3cba8fc7
Arjane JAMK <ad1945@student.jamk.fi>
2022-12-05 20:06:13 +0200

    Added xp goals and checks

--- AllFunctions.py
+++ AllFunctions.py
@@ -18,10 +18,20 @@ class Character:
         return f"\n\nYour character's statistics are:\n\nName: {self.name}\nGender: {self.gender}\nDay: {stats_list[0]}\nGold: {stats_list[1]}\nXP: {stats_list[2]}\nLevel: {stats_list[3]}" # of {mission_days} quest days" --> add this later
 
     # increase the xp of the character, and level up if needed
-    def gain_xp(self, xp_increase: int):
-        self.char_status["xp"] += xp_increase
-        if self.char_status["xp"] % 8 == 0: # check if level-up is needed
-            self.char_status["level"] = round(self.char_status["xp"] / 8) # level up with the correct amount of levels
+    def gain_animal_xp(self, animal_xp_increase: int):
+        self.char_status["animal xp"] += animal_xp_increase
+        if self.char_status["animal xp"] % 8 == 0: # check if level-up is needed # IS THIS STILL RELEVANT?????? <--------------------^^^*******ÄÄÄÄ
+            self.char_status["level"] = round(self.char_status["animal xp"] / 8) # level up with the correct amount of levels
+
+    def gain_dex_xp(self, dex_xp_increase: int):
+        self.char_status["dex xp"] += dex_xp_increase
+        if self.char_status["dex xp"] % 8 == 0: # check if level-up is needed
+            self.char_status["level"] = round(self.char_status["dex xp"] / 8) # level up with the correct amount of levels
+
+    def gain_entertainment_xp(self, entertainment_xp_increase: int):
+        self.char_status["entertainment xp"] += entertainment_xp_increase
+        if self.char_status["entertainment xp"] % 8 == 0: # check if level-up is needed
+            self.char_status["level"] = round(self.char_status["entertainment xp"] / 8) # level up with the correct amount of levels                        
 
 ##########################################################################
 #                           CHARACTER CREATION                           #
@@ -39,12 +49,63 @@ def create_character():
         else:
             break
         
-    char_status = {"day": 0, "gold": 0, "xp": 0, "level": 0} # create the statistics of the character
+    char_status = {"day": 0, "gold": 0, "animal xp": 0, "dex xp": 0, "entertainment xp": 0, "level": 0} # create the statistics of the character
 
 # create a character "mycharacter"
     mycharacter = Character(name, gender, char_status)
     return mycharacter
 
 ##########################################################################
-#                           NEXT FUNCTION                                #
+#                                GOAL                                    #
 ##########################################################################
+
+class Goal:
+
+    def __init__(self, goal_name: str,):
+        self.goal_name = goal_name
+        
+    def goal_values(self, goal_name):
+        if goal_name == "buy horse":
+            animal_xp_goal = 30
+            dex_xp_goal = 10
+            entertainment_xp_goal = 0
+            gold_goal = 300
+
+        elif goal_name == "get own house":
+            animal_xp_goal = 0
+            dex_xp_goal = 50
+            entertainment_xp_goal = 0
+            gold_goal = 400
+
+        elif goal_name == "buy lute":
+            animal_xp_goal = 0
+            dex_xp_goal = 10
+            entertainment_xp_goal = 60
+            gold_goal = 200
+        
+        return animal_xp_goal, dex_xp_goal, entertainment_xp_goal, gold_goal
+
+    def goal_reached(self, animal_xp_goal: int, dex_xp_goal: int, entertainment_xp_goal: int, gold_goal: int): # get goal values to check against acquired gold and xp
+        if animal_xp_goal >= self.char_status["animal xp"]:
+            animal_goal_reached = True
+        else:
+            animal_goal_reached = False
+
+        if dex_xp_goal >= self.char_status["dex xp"]:
+            dex_goal_reached = True
+        else:
+            dex_goal_reached = False
+
+        if entertainment_xp_goal >= self.char_status["entertainment xp"]:
+            entertainment_goal_reached = True
+        else:
+            entertainment_goal_reached = False
+
+        if gold_goal >= self.char_status["gold"]:
+            gold_goal_reached = True
+
+        if animal_goal_reached == True and dex_goal_reached == True and entertainment_goal_reached == True and gold_goal_reached = True:
+            game_won()
+
+        else:
+            game_lost()
